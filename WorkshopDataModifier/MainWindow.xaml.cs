b35a63c56145ad92e842442e057e4bc365fa367a
Enzoolino <krzysiekwronapl@wp.pl>
2023-05-31 16:48:20 +0200

    Sketch of the Main application window. Added themes to the navigation panel buttons and added FontsAwesome Nuget Package.

---
+++ WorkshopDataModifier/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+
+namespace WorkshopDataModifier
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+    }
+}
