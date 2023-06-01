8a2a7b1e8df2f78c1ec734ab09e637901a1871f5
Enzoolino <krzysiekwronapl@wp.pl>
2023-06-02 00:04:33 +0200

    Implemented basic MVVM system for 2 views, added a few more templates to style the UI and moved some content from MainWindow to HomeView.

---
+++ WorkshopDataModifier/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using WorkshopDataModifier.Core;
+
+namespace WorkshopDataModifier.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand ClientsViewCommand { get; set; }
+
+        public HomeViewModel HomeVM { get; set; }
+        public ClientsViewModel ClientsVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            ClientsVM = new ClientsViewModel();
+
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            ClientsViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = ClientsVM;
+            });
+
+
+        }
+    }
+}
--- WorkshopDataModifier/MainWindow.xaml.cs
+++ WorkshopDataModifier/MainWindow.xaml.cs
@@ -22,5 +22,27 @@ namespace WorkshopDataModifier
                 this.DragMove();
             }
         }
+
+        private bool IsMaximized = false;
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                if (IsMaximized)
+                {
+                    this.WindowState = WindowState.Normal;
+                    this.Width = 1280;
+                    this.Height = 740;
+
+                    IsMaximized = false;
+                }
+                else
+                {
+                    this.WindowState = WindowState.Maximized;
+
+                    IsMaximized = true;
+                }
+            }
+        }
     }
 }
