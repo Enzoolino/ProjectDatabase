f9e094c299e69dd150c5a1898715a8b0af7f7a50
Enzoolino <krzysiekwronapl@wp.pl>
2023-06-10 17:54:06 +0200

    Added new nuget 'Material Design' and created login page for the application.

--- WorkshopDataModifier/App.xaml.cs
+++ WorkshopDataModifier/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using WorkshopDataModifier.MVVM.View.Login;
 
 namespace WorkshopDataModifier
 {
@@ -13,5 +14,13 @@ namespace WorkshopDataModifier
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.Show();
+        }
+
     }
 }
