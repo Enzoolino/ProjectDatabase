7244944c963bc4cb607a5debaeae469f98ddfaa6
Enzoolino <krzysiekwronapl@wp.pl>
2023-06-16 20:01:32 +0200

    Updated UI of 'DealershipsView'

--- WorkshopDataModifier/MVVM/View/DealershipsView.xaml.cs
+++ WorkshopDataModifier/MVVM/View/DealershipsView.xaml.cs
@@ -1,5 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Validation;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +17,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WorkshopDataModifier.Data;
 
 namespace WorkshopDataModifier.MVVM.View
 {
@@ -20,9 +26,556 @@ namespace WorkshopDataModifier.MVVM.View
     /// </summary>
     public partial class DealershipsView : UserControl
     {
+
+        #region Counter of the current clients (dynamic)
+        private int _rowCount;
+        public int RowCount
+        {
+            get { return _rowCount; }
+            set
+            {
+                _rowCount = value;
+                OnPropertyChanged(nameof(RowCount));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        #region Multi Select
+        private void SelectAllCheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            bool isChecked = ((CheckBox)sender).IsChecked == true;
+
+            foreach (dealership row in DealershipsDataGrid.Items)
+            {
+                row.IsSelected = isChecked;
+            }
+        }
+
+        private void RowCheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            DataGridRow row = (DataGridRow)DealershipsDataGrid.ItemContainerGenerator.ContainerFromItem(((FrameworkElement)sender).DataContext);
+            if (row != null)
+            {
+                dealership rowData = (dealership)row.Item;
+                rowData.IsSelected = ((CheckBox)sender).IsChecked == true;
+            }
+        }
+        #endregion
+
+        #region Data Modification
+
+        //List of all selected rows (Initialized with button click)
+        static List<dealership> selectedRows = new List<dealership>();
+
+        #region Edit Section
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (dealership rowData in DealershipsDataGrid.Items)
+            {
+                if (rowData.IsSelected)
+                {
+                    selectedRows.Add(rowData);
+                }
+            }
+
+            Button editButton = (Button)sender;
+            dealership row = (dealership)editButton.DataContext;
+
+            if (selectedRows.Count == 0)
+                selectedRows.Add(row);
+
+            if (!selectedRows.Contains(row))
+            {
+                selectedRows.Clear();
+                return;
+            }
+
+            if (selectedRows.Count == 0)
+            {
+                throw new Exception("Row was not selected or something went wrong");
+            }
+            else if (selectedRows.Count == 1)
+            {
+                MultiEditionWarning.Visibility = Visibility.Collapsed;
+
+                EditName.Text = row.Name;
+                EditLocation.Text = row.Location;
+                EditBranch.Text = row.BranchID.ToString();
+
+                EditPopup.IsOpen = true;
+            }
+            else
+            {
+                EditPopup.DataContext = selectedRows;
+                MultiEditionWarning.Visibility = Visibility.Visible;
+
+                EditPopup.IsOpen = true;
+            }
+
+        }
+
+        private void ConfirmEditButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (var context = new DealershipsDbContext())
+                {
+                    if (selectedRows.Count == 0)
+                    {
+                        throw new Exception("Row was not selected or something went wrong");
+                    }
+                    else if (selectedRows.Count == 1)
+                    {
+                        dealership selectedRow = context.Dealership.Find(selectedRows[0].Name);
+
+                        int txtBranch = int.Parse(EditBranch.Text);
+
+                        selectedRow.Name  = EditName.Text;
+                        selectedRow.Location = EditLocation.Text;
+                        selectedRow.BranchID = txtBranch;
+                    }
+                    else
+                    {
+                        foreach (dealership dataRow in selectedRows)
+                        {
+                            dealership selectedRow = context.Dealership.Find(dataRow.Name);
+
+                            int txtBranch = int.Parse(EditBranch.Text);
+
+                            if (EditLocation.Text != "" && EditLocation.Text != null)
+                                selectedRow.Location = EditLocation.Text;
+
+                            if (EditBranch.Text != "" && EditBranch.Text != null)
+                                selectedRow.BranchID = txtBranch;
+                        }
+                    }
+
+                    context.SaveChanges();
+                    DealershipsDataGrid.ItemsSource = context.Dealership.ToList();
+                }
+                EditPopup.IsOpen = false;
+
+                EditName.Text = "";
+                EditLocation.Text = "";
+                EditBranch.Text = "";
+
+                selectedRows.Clear();
+            }
+            catch (DbEntityValidationException ex)
+            {
+                // Handle validation errors
+                StringBuilder errorMessage = new StringBuilder();
+                foreach (var validationErrors in ex.EntityValidationErrors)
+                {
+                    foreach (var validationError in validationErrors.ValidationErrors)
+                    {
+                        errorMessage.AppendLine($"Error: {validationError.ErrorMessage}");
+                    }
+                }
+
+                MessageBox.Show(errorMessage.ToString(), "Validation Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (Exception ex)
+            {
+                // Handle other database errors
+                MessageBox.Show($"An error occurred while saving changes: {ex.Message}", "Database Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void CancelEditButton_Click(object sender, RoutedEventArgs e)
+        {
+            EditName.Text = "";
+            EditLocation.Text = "";
+            EditBranch.Text = "";
+
+            selectedRows.Clear();
+
+            EditPopup.IsOpen = false;
+        }
+        #endregion
+
+        #region Delete Section
+        private void RemoveButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (dealership rowData in DealershipsDataGrid.Items)
+            {
+                if (rowData.IsSelected)
+                {
+                    selectedRows.Add(rowData);
+                }
+            }
+
+            Button removeButton = (Button)sender;
+            dealership row = (dealership)removeButton.DataContext;
+
+            if (selectedRows.Count == 0)
+                selectedRows.Add(row);
+
+            if (!selectedRows.Contains(row))
+            {
+                selectedRows.Clear();
+                return;
+            }
+
+            if (selectedRows.Count == 0)
+            {
+                throw new Exception("Row was not selected or something went wrong");
+            }
+            else if (selectedRows.Count == 1)
+            {
+                DeletePopup.IsOpen = true;
+            }
+            else
+            {
+                DeletePopup.DataContext = selectedRows;
+                DeletePopup.IsOpen = true;
+            }
+        }
+
+        private void ConfirmRemoveButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (var context = new DealershipsDbContext())
+                {
+                    if (selectedRows.Count == 0)
+                    {
+                        throw new Exception("Row was not selected or something went wrong");
+                    }
+                    else if (selectedRows.Count == 1)
+                    {
+                        dealership selectedRow = context.Dealership.Find(selectedRows[0].Name);
+                        context.Dealership.Remove(selectedRow);
+                    }
+                    else
+                    {
+                        foreach (dealership dataRow in selectedRows)
+                        {
+                            dealership selectedRow = context.Dealership.Find(dataRow.Name);
+                            context.Entry(selectedRow).State = EntityState.Deleted;
+                        }
+                    }
+
+                    //Update DataGrid to show changes
+                    context.SaveChanges();
+                    DealershipsDataGrid.ItemsSource = context.Dealership.ToList();
+                }
+
+                RowCount = DealershipsDataGrid.Items.Count;
+                DealershipsCounter.Text = $"Current Saved Dealerships: {RowCount}";
+
+                DeletePopup.IsOpen = false;
+                selectedRows.Clear();
+            }
+            catch (DbUpdateException ex)
+            {
+                // Handle specific database exceptions
+                if (ex.InnerException is SqlException sqlException)
+                {
+                    // Handle referential integrity constraint violation
+                    if (sqlException.Number == 547)
+                    {
+                        MessageBox.Show("Cannot delete the item because it is referenced by other entities.", "Delete Error", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK, MessageBoxOptions.DefaultDesktopOnly);
+                    }
+                    else
+                    {
+                        MessageBox.Show($"An error occurred while deleting the item: {sqlException.Message}", "Delete Error", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK, MessageBoxOptions.DefaultDesktopOnly);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show($"An error occurred while deleting the item: {ex.Message}", "Delete Error", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK, MessageBoxOptions.DefaultDesktopOnly);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle other exceptions
+                MessageBox.Show($"An error occurred while deleting the item: {ex.Message}", "Delete Error", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK, MessageBoxOptions.DefaultDesktopOnly);
+            }
+        }
+
+        private void CancelRemoveButton_Click(object sender, RoutedEventArgs e)
+        {
+            selectedRows.Clear();
+            DeletePopup.IsOpen = false;
+        }
+        #endregion
+
+        #region Add Section
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            AddPopup.IsOpen = true;
+        }
+
+        private void ConfirmAddButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (var context = new DealershipsDbContext())
+                {
+                    int txtBranch = int.Parse(AddBranch.Text);
+
+                    dealership newBranch = new dealership
+                    {
+                        Name = AddName.Text,
+                        Location = AddLocation.Text,
+                        BranchID = txtBranch
+                    };
+
+                    context.Dealership.Add(newBranch);
+                    context.SaveChanges();
+
+                    DealershipsDataGrid.ItemsSource = context.Dealership.ToList();
+                }
+
+                RowCount = DealershipsDataGrid.Items.Count;
+                DealershipsCounter.Text = $"Current Saved Dealerships: {RowCount}";
+                AddPopup.IsOpen = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred while adding the client: {ex.Message}", "Add Error", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK, MessageBoxOptions.DefaultDesktopOnly);
+            }
+        }
+
+        private void CancelAddButton_Click(object sender, RoutedEventArgs e)
+        {
+            AddName.Text = "";
+            AddLocation.Text = "";
+            AddBranch.Text = "";
+
+            AddPopup.IsOpen = false;
+        }
+
+        #endregion
+
+        #endregion 
+
+        #region Draggable Popups
+
+        private bool isDraggingPopup = false;
+        private Point startPoint;
+
+        #region Add Popup
+        private void AddPopup_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                isDraggingPopup = true;
+                startPoint = e.GetPosition(AddPopup);
+            }
+        }
+
+        private void AddPopup_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDraggingPopup)
+            {
+                Point currentPoint = e.GetPosition(AddPopup);
+                double offsetX = currentPoint.X - startPoint.X;
+                double offsetY = currentPoint.Y - startPoint.Y;
+
+                AddPopup.HorizontalOffset += offsetX;
+                AddPopup.VerticalOffset += offsetY;
+
+                startPoint = currentPoint;
+            }
+        }
+
+        private void AddPopup_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                isDraggingPopup = false;
+            }
+        }
+        #endregion
+
+        #region Edit Popup
+        private void EditPopup_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                isDraggingPopup = true;
+                startPoint = e.GetPosition(EditPopup);
+            }
+        }
+
+        private void EditPopup_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDraggingPopup)
+            {
+                Point currentPoint = e.GetPosition(EditPopup);
+                double offsetX = currentPoint.X - startPoint.X;
+                double offsetY = currentPoint.Y - startPoint.Y;
+
+                EditPopup.HorizontalOffset += offsetX;
+                EditPopup.VerticalOffset += offsetY;
+
+                startPoint = currentPoint;
+            }
+        }
+
+        private void EditPopup_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                isDraggingPopup = false;
+            }
+        }
+        #endregion
+
+        #region Delete Popup
+        private void DeletePopup_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                isDraggingPopup = true;
+                startPoint = e.GetPosition(DeletePopup);
+            }
+        }
+
+        private void DeletePopup_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDraggingPopup)
+            {
+                Point currentPoint = e.GetPosition(DeletePopup);
+                double offsetX = currentPoint.X - startPoint.X;
+                double offsetY = currentPoint.Y - startPoint.Y;
+
+                DeletePopup.HorizontalOffset += offsetX;
+                DeletePopup.VerticalOffset += offsetY;
+
+                startPoint = currentPoint;
+            }
+        }
+
+        private void DeletePopup_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                isDraggingPopup = false;
+            }
+        }
+        #endregion
+
+        #endregion
+
+        #region Search
+
+        private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txtSearchDealerships.Text != "Search in Dealerships...")
+            {
+                string searchText = txtSearchDealerships.Text.ToLower();
+
+                DealershipsDataGrid.Items.Filter = item =>
+                {
+                    if (item is dealership dataItem)
+                    {
+                        string txtBranch = dataItem.BranchID.ToString();
+
+                        return dataItem.Name.ToLower().Contains(searchText) ||
+                               dataItem.Location.ToLower().Contains(searchText) ||
+                               txtBranch.Contains(searchText);
+                    }
+
+                    return false;
+                };
+
+                DealershipsDataGrid.Items.Refresh();
+            }
+        }
+
+        #endregion
+
+        #region Focus Settings
+
+        private void DealershipsWindow_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (txtSearchDealerships.Text == "")
+            {
+                Keyboard.ClearFocus();
+                txtSearchDealerships.Text = "Search in Dealerships...";
+            }
+            else
+            {
+                Keyboard.ClearFocus();
+            }
+
+        }
+
+        private void txtSearchDealerships_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            if (txtSearchDealerships.Text == "Search in Dealerships...")
+            {
+                txtSearchDealerships.Text = "";
+            }
+        }
+        #endregion
+
+        #region Date Filtering
+
+        private void DataFilterButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (DayButton.IsChecked == true)
+            {
+
+            }
+            else if (WeekButton.IsChecked == true)
+            {
+
+            }
+            else if (MonthButton.IsChecked == true)
+            {
+
+            }
+            else if (YearButton.IsChecked == true)
+            {
+
+            }
+            else if (InfButton.IsChecked == true)
+            {
+
+            }
+
+        }
+        #endregion
+
+
+        private DealershipsDbContext _dbContext;
         public DealershipsView()
         {
             InitializeComponent();
+
+            //Database initializer
+            _dbContext = new DealershipsDbContext();
+            DealershipsDataGrid.ItemsSource = _dbContext.Dealership.ToList();
+
+            //Counter initializer
+            RowCount = DealershipsDataGrid.Items.Count;
+            DealershipsCounter.Text = $"Current Saved Dealerships: {RowCount}";
         }
     }
+
+
+    /// <summary>
+    /// Gets context of dealership tab from the connected DataBase
+    /// </summary>
+    public class DealershipsDbContext : DbContext
+    {
+        public DbSet<dealership> Dealership { get; set; } //DbSet dla tabeli "dealership"
+
+        public DealershipsDbContext() : base("DealershipCon")
+        {
+        }
+    }
+
 }
