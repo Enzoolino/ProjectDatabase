8f91ac89cd77a379811b74e398b44553229cfc0c
Enzoolino <krzysiekwronapl@wp.pl>
2023-06-17 14:38:38 +0200

    Update UI of 'PositionsView'.

--- WorkshopDataModifier/MVVM/View/PositionsView.xaml.cs
+++ WorkshopDataModifier/MVVM/View/PositionsView.xaml.cs
@@ -1,5 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Validation;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +17,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WorkshopDataModifier.Data;
 
 namespace WorkshopDataModifier.MVVM.View
 {
@@ -20,9 +26,524 @@ namespace WorkshopDataModifier.MVVM.View
     /// </summary>
     public partial class PositionsView : UserControl
     {
+
+        #region Counter of the current clients (dynamic)
+        private int _rowCount;
+        public int RowCount
+        {
+            get { return _rowCount; }
+            set
+            {
+                _rowCount = value;
+                OnPropertyChanged(nameof(RowCount));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        #region Multi Select
+        private void SelectAllCheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            bool isChecked = ((CheckBox)sender).IsChecked == true;
+
+            foreach (position row in PositionsDataGrid.Items)
+            {
+                row.IsSelected = isChecked;
+            }
+        }
+
+        private void RowCheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            DataGridRow row = (DataGridRow)PositionsDataGrid.ItemContainerGenerator.ContainerFromItem(((FrameworkElement)sender).DataContext);
+            if (row != null)
+            {
+                position rowData = (position)row.Item;
+                rowData.IsSelected = ((CheckBox)sender).IsChecked == true;
+            }
+        }
+        #endregion
+
+        #region Data Modification
+
+        //List of all selected rows (Initialized with button click)
+        static List<position> selectedRows = new List<position>();
+
+        #region Edit Section
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (position rowData in PositionsDataGrid.Items)
+            {
+                if (rowData.IsSelected)
+                {
+                    selectedRows.Add(rowData);
+                }
+            }
+
+            Button editButton = (Button)sender;
+            position row = (position)editButton.DataContext;
+
+            if (selectedRows.Count == 0)
+                selectedRows.Add(row);
+
+            if (!selectedRows.Contains(row))
+            {
+                selectedRows.Clear();
+                return;
+            }
+
+            if (selectedRows.Count == 0)
+            {
+                throw new Exception("Row was not selected or something went wrong");
+            }
+            else if (selectedRows.Count == 1)
+            {
+                MultiEditionWarning.Visibility = Visibility.Collapsed;
+
+                EditName.Text = row.Name;
+
+                EditPopup.IsOpen = true;
+            }
+            else
+            {
+                EditPopup.DataContext = selectedRows;
+                MultiEditionWarning.Visibility = Visibility.Visible;
+
+                EditPopup.IsOpen = true;
+            }
+
+        }
+
+        private void ConfirmEditButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (var context = new PositionsDbContext())
+                {
+                    if (selectedRows.Count == 0)
+                    {
+                        throw new Exception("Row was not selected or something went wrong");
+                    }
+                    else if (selectedRows.Count == 1)
+                    {
+                        position selectedRow = context.Position.Find(selectedRows[0].Name);
+
+                        selectedRow.Name = EditName.Text;
+                    }
+                    else // Con't be multi edited
+                    {
+
+                    }
+
+                    context.SaveChanges();
+                    PositionsDataGrid.ItemsSource = context.Position.ToList();
+                }
+                EditPopup.IsOpen = false;
+
+                EditName.Text = "";
+
+                selectedRows.Clear();
+            }
+            catch (DbEntityValidationException ex)
+            {
+                // Handle validation errors
+                StringBuilder errorMessage = new StringBuilder();
+                foreach (var validationErrors in ex.EntityValidationErrors)
+                {
+                    foreach (var validationError in validationErrors.ValidationErrors)
+                    {
+                        errorMessage.AppendLine($"Error: {validationError.ErrorMessage}");
+                    }
+                }
+
+                MessageBox.Show(errorMessage.ToString(), "Validation Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (Exception ex)
+            {
+                // Handle other database errors
+                MessageBox.Show($"An error occurred while saving changes: {ex.Message}", "Database Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void CancelEditButton_Click(object sender, RoutedEventArgs e)
+        {
+            EditName.Text = "";
+
+            selectedRows.Clear();
+
+            EditPopup.IsOpen = false;
+        }
+        #endregion
+
+        #region Delete Section
+        private void RemoveButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (position rowData in PositionsDataGrid.Items)
+            {
+                if (rowData.IsSelected)
+                {
+                    selectedRows.Add(rowData);
+                }
+            }
+
+            Button removeButton = (Button)sender;
+            position row = (position)removeButton.DataContext;
+
+            if (selectedRows.Count == 0)
+                selectedRows.Add(row);
+
+            if (!selectedRows.Contains(row))
+            {
+                selectedRows.Clear();
+                return;
+            }
+
+            if (selectedRows.Count == 0)
+            {
+                throw new Exception("Row was not selected or something went wrong");
+            }
+            else if (selectedRows.Count == 1)
+            {
+                DeletePopup.IsOpen = true;
+            }
+            else
+            {
+                DeletePopup.DataContext = selectedRows;
+                DeletePopup.IsOpen = true;
+            }
+        }
+
+        private void ConfirmRemoveButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (var context = new PositionsDbContext())
+                {
+                    if (selectedRows.Count == 0)
+                    {
+                        throw new Exception("Row was not selected or something went wrong");
+                    }
+                    else if (selectedRows.Count == 1)
+                    {
+                        position selectedRow = context.Position.Find(selectedRows[0].Name);
+                        context.Position.Remove(selectedRow);
+                    }
+                    else
+                    {
+                        foreach (position dataRow in selectedRows)
+                        {
+                            position selectedRow = context.Position.Find(dataRow.Name);
+                            context.Entry(selectedRow).State = EntityState.Deleted;
+                        }
+                    }
+
+                    //Update DataGrid to show changes
+                    context.SaveChanges();
+                    PositionsDataGrid.ItemsSource = context.Position.ToList();
+                }
+
+                RowCount = PositionsDataGrid.Items.Count;
+                PositionsCounter.Text = $"Current Saved Positions: {RowCount}";
+
+                DeletePopup.IsOpen = false;
+                selectedRows.Clear();
+            }
+            catch (DbUpdateException ex)
+            {
+                // Handle specific database exceptions
+                if (ex.InnerException is SqlException sqlException)
+                {
+                    // Handle referential integrity constraint violation
+                    if (sqlException.Number == 547)
+                    {
+                        MessageBox.Show("Cannot delete the item because it is referenced by other entities.", "Delete Error", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK, MessageBoxOptions.DefaultDesktopOnly);
+                    }
+                    else
+                    {
+                        MessageBox.Show($"An error occurred while deleting the item: {sqlException.Message}", "Delete Error", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK, MessageBoxOptions.DefaultDesktopOnly);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show($"An error occurred while deleting the item: {ex.Message}", "Delete Error", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK, MessageBoxOptions.DefaultDesktopOnly);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle other exceptions
+                MessageBox.Show($"An error occurred while deleting the item: {ex.Message}", "Delete Error", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK, MessageBoxOptions.DefaultDesktopOnly);
+            }
+        }
+
+        private void CancelRemoveButton_Click(object sender, RoutedEventArgs e)
+        {
+            selectedRows.Clear();
+            DeletePopup.IsOpen = false;
+        }
+        #endregion
+
+        #region Add Section
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            AddPopup.IsOpen = true;
+        }
+
+        private void ConfirmAddButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (var context = new PositionsDbContext())
+                {
+                    position newBrand = new position
+                    {
+                        Name = AddName.Text
+                    };
+
+                    context.Position.Add(newBrand);
+                    context.SaveChanges();
+
+                    PositionsDataGrid.ItemsSource = context.Position.ToList();
+                }
+
+                RowCount = PositionsDataGrid.Items.Count;
+                PositionsCounter.Text = $"Current Saved Positions: {RowCount}";
+                AddPopup.IsOpen = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred while adding the client: {ex.Message}", "Add Error", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK, MessageBoxOptions.DefaultDesktopOnly);
+            }
+        }
+
+        private void CancelAddButton_Click(object sender, RoutedEventArgs e)
+        {
+            AddName.Text = "";
+
+            AddPopup.IsOpen = false;
+        }
+
+        #endregion
+
+        #endregion 
+
+        #region Draggable Popups
+
+        private bool isDraggingPopup = false;
+        private Point startPoint;
+
+        #region Add Popup
+        private void AddPopup_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                isDraggingPopup = true;
+                startPoint = e.GetPosition(AddPopup);
+            }
+        }
+
+        private void AddPopup_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDraggingPopup)
+            {
+                Point currentPoint = e.GetPosition(AddPopup);
+                double offsetX = currentPoint.X - startPoint.X;
+                double offsetY = currentPoint.Y - startPoint.Y;
+
+                AddPopup.HorizontalOffset += offsetX;
+                AddPopup.VerticalOffset += offsetY;
+
+                startPoint = currentPoint;
+            }
+        }
+
+        private void AddPopup_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                isDraggingPopup = false;
+            }
+        }
+        #endregion
+
+        #region Edit Popup
+        private void EditPopup_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                isDraggingPopup = true;
+                startPoint = e.GetPosition(EditPopup);
+            }
+        }
+
+        private void EditPopup_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDraggingPopup)
+            {
+                Point currentPoint = e.GetPosition(EditPopup);
+                double offsetX = currentPoint.X - startPoint.X;
+                double offsetY = currentPoint.Y - startPoint.Y;
+
+                EditPopup.HorizontalOffset += offsetX;
+                EditPopup.VerticalOffset += offsetY;
+
+                startPoint = currentPoint;
+            }
+        }
+
+        private void EditPopup_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                isDraggingPopup = false;
+            }
+        }
+        #endregion
+
+        #region Delete Popup
+        private void DeletePopup_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                isDraggingPopup = true;
+                startPoint = e.GetPosition(DeletePopup);
+            }
+        }
+
+        private void DeletePopup_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDraggingPopup)
+            {
+                Point currentPoint = e.GetPosition(DeletePopup);
+                double offsetX = currentPoint.X - startPoint.X;
+                double offsetY = currentPoint.Y - startPoint.Y;
+
+                DeletePopup.HorizontalOffset += offsetX;
+                DeletePopup.VerticalOffset += offsetY;
+
+                startPoint = currentPoint;
+            }
+        }
+
+        private void DeletePopup_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                isDraggingPopup = false;
+            }
+        }
+        #endregion
+
+        #endregion
+
+        #region Search
+
+        private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txtSearchPositions.Text != "Search in Positions...")
+            {
+                string searchText = txtSearchPositions.Text.ToLower();
+
+                PositionsDataGrid.Items.Filter = item =>
+                {
+                    if (item is position dataItem)
+                    {
+                        return dataItem.Name.ToLower().Contains(searchText);
+                    }
+
+                    return false;
+                };
+
+                PositionsDataGrid.Items.Refresh();
+            }
+        }
+
+        #endregion
+
+        #region Focus Settings
+
+        private void PositionsWindow_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (txtSearchPositions.Text == "")
+            {
+                Keyboard.ClearFocus();
+                txtSearchPositions.Text = "Search in Positions...";
+            }
+            else
+            {
+                Keyboard.ClearFocus();
+            }
+
+        }
+
+        private void txtSearchPositions_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            if (txtSearchPositions.Text == "Search in Positions...")
+            {
+                txtSearchPositions.Text = "";
+            }
+        }
+        #endregion
+
+        #region Date Filtering
+
+        private void DataFilterButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (DayButton.IsChecked == true)
+            {
+
+            }
+            else if (WeekButton.IsChecked == true)
+            {
+
+            }
+            else if (MonthButton.IsChecked == true)
+            {
+
+            }
+            else if (YearButton.IsChecked == true)
+            {
+
+            }
+            else if (InfButton.IsChecked == true)
+            {
+
+            }
+
+        }
+        #endregion
+
+
+        private PositionsDbContext _dbContext;
         public PositionsView()
         {
             InitializeComponent();
+
+            //Database initializer
+            _dbContext = new PositionsDbContext();
+            PositionsDataGrid.ItemsSource = _dbContext.Position.ToList();
+
+            //Counter initializer
+            RowCount = PositionsDataGrid.Items.Count;
+            PositionsCounter.Text = $"Current Saved Positions: {RowCount}";
+        }
+    }
+
+
+    /// <summary>
+    /// Gets context of position tab from the connected DataBase
+    /// </summary>
+    public class PositionsDbContext : DbContext
+    {
+        public DbSet<position> Position { get; set; } //DbSet for table "position"
+
+        public PositionsDbContext() : base("DealershipCon")
+        {
         }
     }
 }
