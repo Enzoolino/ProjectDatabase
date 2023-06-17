6cd04d0b7296dc0c7c062911eb7843c4c76db96f
Enzoolino <krzysiekwronapl@wp.pl>
2023-06-17 18:22:35 +0200

    Moved the Data Model to 'MVVM/Model' folder.

--- WorkshopDataModifier/MVVM/Model/DealershipModel.Designer.cs
+++ WorkshopDataModifier/MVVM/Model/DealershipModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'A:\Microsoft Visual Studio\Repos\ProjectDatabase\WorkshopDataModifier\Data\DealershipModel.edmx'. 
+﻿// T4 code generation is enabled for model 'A:\Microsoft Visual Studio\Repos\ProjectDatabase\WorkshopDataModifier\MVVM\Model\DealershipModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
