15bd1bb1f36503bc415a16dd98f80d5d443dc0ec
Enzoolino <krzysiekwronapl@wp.pl>
2023-06-23 15:47:03 +0200

    Updated UI of the homepage and created a simple animation.

--- WorkshopDataModifier/MVVM/View/Home/HomeView.xaml.cs
+++ WorkshopDataModifier/MVVM/View/Home/HomeView.xaml.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Validation;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using WorkshopDataModifier.MVVM.Model;
 
 
 namespace WorkshopDataModifier.MVVM.View
@@ -20,6 +23,33 @@ namespace WorkshopDataModifier.MVVM.View
         public HomeView()
         {
             InitializeComponent();
+
+            HomeDate.Text = $"Today is {DateTime.Now.ToString("dd/MM/yyyy")}";
+
+            // Retrieve the number of sold vehicles from the database
+            using (var context = new HomeDbContext())
+            {
+                int numberOfSoldVehicles = context.SoldVehicle.Count();
+                int numberOfOpenedDealerships = context.Dealership.Count();
+
+                // Update the Number property of the InfoCard controls
+                SoldVehiclesCard.Number = numberOfSoldVehicles.ToString();
+                OpenedDealershipsCard.Number = numberOfOpenedDealerships.ToString();
+            }
+
+        }
+
+        /// <summary>
+        /// Gets context of sold vahicles tab from the connected DataBase
+        /// </summary>
+        public class HomeDbContext : DbContext
+        {
+            public DbSet<sold_vehicles> SoldVehicle { get; set; }
+            public DbSet<dealership> Dealership { get; set; }
+            
+            public HomeDbContext() : base("DealershipCon")
+            {
+            }
         }
 
     }
